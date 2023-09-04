e2155c7b449cd3d2435b74f5d3006f741da5b032
khanhuyengiang <khanhuyengiang@gmail.com>
2023-09-04 22:25:08 +0800

    add fpga noise

--- Kay/simple_simulation.py
+++ Kay/simple_simulation.py
@@ -1,7 +1,18 @@
 from qutip_qip.compiler import GateCompiler, Instruction
 from qutip_qip.device import ModelProcessor
-from qutip import (sigmax, sigmay)
+from qutip import (sigmax, sigmay, tensor)
+
+import functools # for reduce
+from qutip.qip.operations.gates import *
+from qutip import basis
+from qutip_qip.circuit import QubitCircuit
+from qutip.metrics import fidelity
+from qutip.qip.noise import RandomNoise
+
 import numpy as np
+
+import sys
+from clifford_inverse_search import gates_set, matrix_list, add_inverse_gates
 
 class simple_processor(ModelProcessor):
     """
@@ -106,3 +117,49 @@ class simple_compiler(GateCompiler):
             return self.generate_pulse(gate, tlist = coupling_time_series, coeff = FPGA_voltage, phase=0.0)
         elif gate.name == "RY":
             return self.generate_pulse(gate, tlist = coupling_time_series, coeff = FPGA_voltage, phase=np.pi / 2)
+
+def single_simple_simulation(num_gates, t1 = None, t2 = None, num_qubits = 1, add_FPGA_noise = True):
+    """
+    A single simulation, with num_gates representing the number of rotations.
+
+    Args:
+        num_gates (int): The number of random gates to add in the simulation.
+        t1, t2 (float): Decoherence time of the qubits.
+        add_FPGA_noise (bool): Whether to add in gaussian FPGA noise to the simulation.
+
+    Returns:
+        final_fidelity (float):
+            Fidelity of the result state obtained from 
+            mesolve solver method and the initial state.
+    """
+     
+    myprocessor = simple_processor(num_qubits, t1 = t1, t2 = t2)
+    myprocessor.native_gates = None  # Remove the native gates
+    mycompiler = simple_compiler(num_qubits,{"pulse_amplitude": 1})
+
+    # Ground state for n qubits
+    init_state = functools.reduce(lambda a, b: tensor(a,b), [basis(2, 0)] * num_qubits)
+
+    # Define a random circuit.
+    circuit = QubitCircuit(num_qubits)
+    clifford = rx(0)
+    for ind in np.random.randint(0, 6, num_gates):
+        circuit.add_gate(gates_set[ind])
+        clifford = matrix_list[ind] * clifford
+
+    # Finding inverse Clifford for the random sequence of gate
+    add_inverse_gates(clifford, init_state, circuit = circuit)
+
+    # Simulate the circuit.
+    myprocessor.load_circuit(circuit, compiler=mycompiler)
+    
+    # FPGA gaussian noise
+    if add_FPGA_noise == True:
+        FPGA_noise = RandomNoise(dt=0.01, indices = [0,1], rand_gen=np.random.normal, loc=0.00, scale=0.3)
+        myprocessor.add_noise(FPGA_noise)
+    
+    # Compute results of the run using a solver of choice
+    result = myprocessor.run_state(init_state, solver="mesolve")
+    # Measured fidelity at the end
+    final_fidelity = fidelity(result.states[0],result.states[-1])
+    return final_fidelity
